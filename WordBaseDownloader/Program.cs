aebd56bcae1714a43d3195faf0ac0f76d7e12587
Alexandr Gorbach <alexandr.gorbach@ximxim.com>
2017-09-27 11:55:23 +0300

    Добавьте файлы проекта.

---
+++ WordBaseDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordBaseDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
